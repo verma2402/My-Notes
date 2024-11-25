c377a0d14b21d3a4773c570ebd9bd953c6d66f46
unknown <verma2402>
2024-11-25 15:53:09 +0530

    update

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import {BASE_URL} from "./constants";
 
 const axiosInstance = axios.create({
     baseURL : BASE_URL , 
-    timeout : 10000,
+    timeout : 30000,
     headers :{
         "Content-Type" : "application/json" ,
     },
